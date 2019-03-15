ee178084c54b8f08372ddf5f04b546a53fb6750e
Eliana Levenson <elianalevenson@Elianas-MacBook-Air.local>
2019-03-15 13:57:47 -0700

    update models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Destination, Restaurant, Hotel, Event, Attraction
 
 # Register your models here.
+admin.site.register(Destination)
+admin.site.register(Restaurant)
+admin.site.register(Hotel)
+admin.site.register(Attraction)
+admin.site.register(Event)
--- main_app/models.py
+++ main_app/models.py
@@ -1,14 +1,80 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+class Airport(models.Model):
+    name = models.CharField(max_length = 100)
+    IATA_code = models.CharField(max_length = 3)
+
+    def __str__(self):
+        return f"{self.name} // {self.IATA_code}"
+
+class Destination(models.Model):
+    departure_date = models.DateField()
+    return_date = models.DateField()
+    # destination_airport = models.ForeignKey(Airport, on_delete=models.CASCADE)
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"Depart Date: {self.departure_date}, Return Date: {self.return_date}, Destination_Airport: {self.destination_airport}, Price: {self.price}"
+
+class Hotel(models.Model):
+    name = models.CharField(max_length = 250)
+    address = models.CharField(max_length = 250)
+    check_in = models.DateField ()
+    check_out = models.DateField ()
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name} // {self.price}"
+
 class Trip(models.Model):
     base_city = models.CharField(max_length=100)
     budget = models.IntegerField()
-    date_from = models.DateField('From')
-    date_to = models.DateField('To')
-
+    # hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    # destination = models.ForeignKey(Destination, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
-        return f"base_city: {self.base_city}, budget: ${budget}, from: {date_from}, to: {date_to}"
+        return f"base_city: {self.base_city}, budget: ${self.budget}"
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length =  250)
+    address = models.CharField(max_length =  250)
+    cuisine = models.CharField(max_length =  250)
+    price = models.FloatField()
+    date_time = models.DateTimeField()
+    # trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} // {self.price}"
+
+class Event (models.Model):
+    name = models.CharField(max_length =  250)
+    address = models.CharField(max_length =  250)
+    price = models.FloatField()
+    date_time = models.DateTimeField()
+    # trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} // {self.price}"
+
+class Attraction (models.Model):
+    name = models.CharField(max_length =  250)
+    address =  models.CharField(max_length =  250)
+    price = models.FloatField()
+    date_time = models.DateTimeField()
+    # trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} // {self.price}"
+
+class DressCode(models.Model):
+    type = models.CharField(max_length = 250)
+
+    def __str__(self):
+        return self.type
+    
+
+
 
