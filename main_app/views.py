88546929176c1bb904ccdcfb5e0ca2f4e1be52ea
Meisam <pmeisam@ymail.com>
2019-03-15 15:56:00 -0700

    updates

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
