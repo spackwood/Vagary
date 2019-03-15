700d9c7a275e55d74250e8bd236b6b76d460aa38
Meisam <pmeisam@ymail.com>
2019-03-15 14:01:30 -0700

    logo update

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+import urllib3
+import xml
+
+skyscanner_api = 'prtl6749387986743898559646983194'
+url = 'http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/FR/eur/en-US/uk/us/anytime/anytime?apikey=prtl6749387986743898559646983194'
+
 # Create your models here.
 class Trip(models.Model):
     base_city = models.CharField(max_length=100)
@@ -8,7 +15,7 @@ class Trip(models.Model):
     date_to = models.DateField('To')
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return f"base_city: {self.base_city}, budget: ${budget}, from: {date_from}, to: {date_to}"
 
