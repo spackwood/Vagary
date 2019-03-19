acaf10634e964a97c5c4f5cc4942ecb6ff0a8ec7
Eliana Levenson <elianalevenson@Elianas-MacBook-Air.local>
2019-03-19 11:46:56 -0700

    add returning flight, departing flight & hotel to trip -- trip created on search if user is logged in

--- main_app/models.py
+++ main_app/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 import urllib3
 import xml
+import datetime
 
 
 # Create your models here.
@@ -15,16 +16,19 @@ class Airport(models.Model):
     def __str__(self):
         return f"{self.name} // {self.IATA_code}"
 
-
-
 class Trip(models.Model):
     name = models.CharField(max_length = 250, default = 'My Trip')
     budget = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    origin = models.CharField(max_length = 3, default = 'LAX')
+    # departure_date = models.DateField(default = datetime.datetime.today())
+    # return_date = models.DateField(default = datetime.datetime.today())
+
     def __str__(self):
         return f"budget: ${self.budget}"
+
     def get_absolute_url(self):
-        return reverse('detail', kwargs={'trip_id': self.id})
+        return reverse('detail', kwargs={'trip_id': self.id, 'airport_code': self.origin})
 
 class Hotel(models.Model):
     name = models.CharField(max_length = 250)
@@ -33,16 +37,17 @@ class Hotel(models.Model):
     check_out = models.CharField(max_length = 100)
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
     price = MoneyField(max_digits=14, decimal_places=2, default_currency='USD')
+    
     def __str__(self):
         return f"{self.name} // {self.price}"
 
 class Flight(models.Model):
     departure_date = models.DateField()
-    # return_date = models.DateField()
     origin = models.CharField(max_length = 3, default = 'LAX')
     destination = models.CharField(max_length = 3, default = 'LAX')
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
     price = MoneyField(max_digits=14, decimal_places=2, default_currency='USD')
+    
     def __str__(self):
         return f"Depart Date: {self.departure_date}, Price: {self.price}, Origin: {self.origin}, Destination: {self.destination}"
 
--- main_app/views.py
+++ main_app/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from django.contrib.auth import authenticate, login
 import os
+from datetime import datetime
 
 from .models import Airport, Trip, Hotel, Flight, User
 
@@ -59,28 +60,29 @@ def destinations(request):
     origin = request.POST.get('origin')
     d_date = request.POST.get('d_date')
     budget = float(request.POST.get('budget'))
+
     if request.user.is_authenticated:
         trip = Trip(budget=budget, user=request.user)
         trip.save()
     else:
         print('no user')
+
     search_list = amadeus.shopping.flight_destinations.get(
         origin=origin,
         departureDate=d_date
         ).data
+
     destinations = []
     for destination in search_list:
         d_price = float(destination['price']['total'])
         if d_price <= budget/2:
             destinations.append(destination)
+
     return render(request, 'destinations/search.html', {'destinations': destinations, 'budget': budget, 'origin': origin, 'departure_date':d_date, "trip": trip})
 
-def hotel_search(request, airport_code):
+def hotel_search(request, trip_id, airport_code):
     budget = float(request.POST.get('budget'))
-    t = request.POST.get('trip')
-    trip = Trip.objects.get(id=t)
-    # destination_estimate = float(request.POST.get('destination_estimate'))
-    # remaining_budget = budget - destination_estimate
+    trip = Trip.objects.get(id=trip_id)
     hotel_search = amadeus.shopping.hotel_offers.get(cityCode=airport_code).data
 # print(json.dumps(hotel_search, indent=2))
     hotels = []
@@ -88,20 +90,21 @@ def hotel_search(request, airport_code):
         h_price = float(hotel['offers'][0]['price']['total'])
         if h_price <= budget/3:
             hotels.append(hotel) 
-    return render(request, 'hotels/search.html', {'hotels' : hotels, 'trip': trip})
+    return render(request, 'hotels/search.html', {'hotels' : hotels, 'trip': trip, 'airport_code': airport_code})
 
-def flight_search(request, airport_code):
+def flight_search(request, trip_id, airport_code):
     budget = float(request.POST.get('budget'))
-    # destination_estimate = float(request.POST.get('destination_estimate'))
-    # print(destination_estimate)
-    # remaining_budget = budget - destination_estimate
     origin = request.POST.get('origin')
     departure_date = request.POST.get('departure_date') or request.POST.get('return_date')
-    t = request.POST.get('trip')
-    flight_search = amadeus.shopping.flight_offers.get(destination=airport_code, origin=origin, departureDate=departure_date).data
-    trip = Trip.objects.get(id=t)
-    flights = []
+    trip = Trip.objects.get(id=trip_id)
 
+    # if return_date:
+    #     return_date = datetime.strptime(departure_date, '%B %d %Y').strftime('%Y-%m-%d')
+    #     flight_search = amadeus.shopping.flight_offers.get(destination=airport_code, origin=origin, departureDate=return_date).data
+    # else:
+    flight_search = amadeus.shopping.flight_offers.get(destination=airport_code, origin=origin, departureDate=departure_date).data
+    
+    flights = []
     for flight in flight_search:
         f_price = float(flight['offerItems'][0]['price']['total'])
         f_origin = flight['offerItems'][0]['services'][0]['segments'][0]['flightSegment']['departure']['iataCode']
@@ -109,19 +112,17 @@ def flight_search(request, airport_code):
         if f_price <= budget/3 and f_origin == origin and f_destination == airport_code:
             flights.append(flight)
     
-    # print(flight_search[0])
 
     return render(request, 'flights/search.html', {'flights': flights, 'trip': trip, 'departure_date': departure_date, 'airport_code': airport_code})
 
-def flight_add(request):
+def flight_add(request, trip_id, airport_code):
     price = request.POST.get('price')
-    print(price)
-    t = request.POST.get('trip')
-    trip = Trip.objects.get(id=t)
-    print(trip)
+    trip = Trip.objects.get(id=trip_id)
     origin = request.POST.get('origin')
     destination = request.POST.get('destination')
     departure_date = request.POST.get('departure_date')
+    airport_code = airport_code
+
     current_flight = Flight.objects.create(
         departure_date = departure_date,
         destination = destination,
@@ -129,18 +130,20 @@ def flight_add(request):
         origin = origin,
         trip = trip,
         )
-    return redirect(f'/trips/{trip.id}')
+
+    return redirect(f'/trips/{trip.id}/{airport_code}')
     # return render(request, 'trip.html', {'current_flight': current_flight})
 
-def hotel_add(request):
+def hotel_add(request, trip_id, airport_code):
     price = request.POST.get('price')
     name = request.POST.get('name')
     check_out = request.POST.get('check_out')
     check_in = request.POST.get('check_in')
     street = request.POST.get('street')
     city = request.POST.get('city')
-    t = request.POST.get('trip')
-    trip = Trip.objects.get(id=t)
+    trip = Trip.objects.get(id=trip_id)
+    origin = airport_code
+
     current_hotel = Hotel.objects.create(
         name = name,
         check_out = check_out,
@@ -150,37 +153,48 @@ def hotel_add(request):
         trip = trip
         )
 
-    # print(current_hotel)
+    print(current_hotel.trip)
     
-    return render(request, 'trip.html', {'current_hotel': current_hotel})
+    return redirect(f'/trips/{trip.id}/{origin}')
 
 class CreateTrip(LoginRequiredMixin, CreateView):
     model = Trip
-    fields = ['name', 'budget']
+    fields = ['name', 'budget', 'origin', 'departure_date', 'return_date']
     def form_valid(self, form):
         form.instance.user = self.request.user    # Let the CreateView do its job as usual
         return super().form_valid(form)
 
-def trips_detail(request, trip_id):
+def trips_detail(request, trip_id, airport_code):
     trip = Trip.objects.get(id=trip_id)
-    # depart_flight=""
-    
+    print(trip)
+    origin = airport_code
+   
     try:
-        depart_flight = Flight.objects.get(trip=trip)
+        depart_flight = Flight.objects.get(trip=trip, origin=origin)
         print('true')
     except:
         depart_flight = None
         print('false')
 
+    try: 
+        return_flight = Flight.objects.get(trip=trip, destination=origin)
+        print('true')
+    except:
+        return_flight = None
+        print('false')
+
     try:
         hotel = Hotel.objects.get(trip=trip)
+        print('true')
     except:
         hotel = None
+        print('false')
     
     return render(request, 'trips/detail.html', {
         'trip': trip,
         'hotel': hotel,
-        'depart_flight': depart_flight
+        'depart_flight': depart_flight, 
+        'return_flight': return_flight,
     })
 
 class TripList(ListView):
